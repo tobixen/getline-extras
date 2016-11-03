dd10b789c84e02f76e0f246b735db23c7a0c5128
Tobias Brox <tobias@redpill-linpro.com>
2016-11-03 22:28:29 +0100

    PUT must use basic auth, no cookie

--- invest.py
+++ invest.py
@@ -3,9 +3,9 @@
 import os
 import requests
 
-cookie={'session': os.environ['GETLINE_COOKIE']}
+auth=(os.environ['GETLINE_USERNAME'], os.environ['GETLINE_PASSWORD'])
 
 def invest(user_id, rate, limit):
-    return requests.put('https://getline.in/api/v1/friends/%s/' % user_id, cookies=cookie, data = {'limit': limit*10**8, 'interest': rate})
+    return requests.put('https://getline.in/api/v1/friends/%s/' % user_id, auth=auth, data = {'limit': int(limit*10**8), 'interest': rate})
 
 
