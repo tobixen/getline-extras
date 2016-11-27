fd3f935fbb1604f684e42aa0ec8d8f5010091ef1
Tobias Brox <tobias@redpill-linpro.com>
2016-11-27 12:16:32 +0100

    merge conflict handling

--- invest.py
+++ invest.py
@@ -6,6 +6,8 @@ import requests
 auth=(os.environ['GETLINE_USERNAME'], os.environ['GETLINE_PASSWORD'])
 
 def invest(user_id, rate, limit):
-    return requests.put('https://getline.in/api/v1/friends/%s/' % user_id, auth=auth, data = {'limit': int(limit*10**8), 'interest': rate})
-
+    if limit<20: ## limit is surely given as bitcoins, not as satoshis.
+        limit *= 10**8
+        limit = int(limit)
+    return requests.put('https://getline.in/api/v1/friends/%s/' % user_id, auth=auth, data = {'limit': limit, 'interest': rate})
 
